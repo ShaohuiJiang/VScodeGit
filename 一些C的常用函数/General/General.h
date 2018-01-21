75776c03887e7fa98402ae879de32d50cf4d9250
ShaohuiJiang <784336623@qq.com>
2018-01-21 19:07:57 +0800

    增加了头文件General.h

---
+++ 一些C的常用函数/General/General.h
@@ -0,0 +1,51 @@
+/*******************************************************************************
+* @file      : General.h
+* @author    : Jiangshaohui
+* @version   : V1.0.0
+* @date      : Sun Jan 21 2018
+* @brief     : 
+********************************************************************************
+* @attention :
+*
+*
+*/
+/* 条件编译-------------------------------------------------------------------*/
+#ifndef __xxx_H
+#define __xxx_H
+
+
+
+/*头文件----------------------------------------------------------------------*/
+///添加头文件
+
+
+
+
+/*宏定义----------------------------------------------------------------------*/
+///添加宏定义
+
+
+
+/*声明外部函数----------------------------------------------------------------*/
+///声明对应C文件中的全局函数
+extern void Delay( unsigned int i );
+extern unsigned char ChkNum( unsigned char *Ptr, int Len );
+extern unsigned char ChkNumAdd( unsigned char *Ptr, int Len );
+extern unsigned char Byte_BCD(unsigned char Data);
+extern void Word_BCD(unsigned char *Ptr, unsigned int Data);
+extern void Long_BCD3(unsigned char *Ptr, unsigned long Data);
+extern void Long_BCD4(unsigned char *Ptr, unsigned long Data);
+extern int IsBCD(unsigned char Data);
+extern unsigned char BCD_Byte(unsigned char Data);
+extern unsigned int BCD2_Word(unsigned char *Ptr);
+extern unsigned long BCD3_Long(unsigned char *Ptr);
+extern unsigned long BCD4_Long(unsigned char *Ptr);
+extern unsigned char Add_Bcd_1(unsigned char n,unsigned char m,unsigned char *carry);
+extern unsigned char Add_Bcd( unsigned char *BufA, unsigned char *BufB, unsigned char Len );
+extern unsigned char Sub_Bcd( unsigned char *BufA, unsigned char *BufB, unsigned char Len );
+extern unsigned char ChkBufZero( unsigned char *Buf,unsigned char Len );
+extern unsigned char Cmp_Data( unsigned char *BufA, unsigned char *BufB, unsigned char Len );
+extern unsigned char Cmp_Bcd_Inv( unsigned char *BcdA, unsigned char *BcdB, unsigned char Len );
+extern void SwapData( unsigned char* Dest, int Len );
+#endif
+/*end------------------------------------------------------------------------*/
